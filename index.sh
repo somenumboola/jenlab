f996c1c92001262a1944aad05843bb9f1eb97c49
Alexandr Glushchenko <somenumboola@gmail.com>
2016-03-20 02:13:04 +0200

    Sketch of sed replacement expression to be used on /etc/gitlab/bitlab.rb to change host to defined by user prompt

--- index.sh
+++ index.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# Sketch of replacement expression to replace host name 
+# with got by prompt to user, will be used for /etc/gitlab/gitlab.rb
+sed -i 's/external_url\(.*\)/external_url "domainof"/g' index.sh 
+
